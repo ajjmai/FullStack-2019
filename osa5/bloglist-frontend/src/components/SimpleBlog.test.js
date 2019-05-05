6c0ff1633dc9720fcfe3c89239c3d4ab75e311c2
Aija Valleala <aija.valleala@aalto.fi>
2019-05-05 18:27:18 +0300

    tehtävä 5.14

--- osa5/bloglist-frontend/src/components/SimpleBlog.test.js
+++ osa5/bloglist-frontend/src/components/SimpleBlog.test.js
@@ -1,7 +1,10 @@
 import React from "react";
-import { render } from "react-testing-library";
+import { render, fireEvent, cleanup } from "react-testing-library";
+import "jest-dom/extend-expect";
 import { prettyDOM } from "dom-testing-library";
 import SimpleBlog from "./SimpleBlog";
+
+afterEach(cleanup);
 
 test("renders content", () => {
   const blog = {
@@ -18,10 +21,30 @@ test("renders content", () => {
   const author = component.container.querySelector(".author");
   //console.log(prettyDOM(title));
   //console.log(prettyDOM(author));
-  expect(author).toBeDefined();
-  expect(title).toBeDefined();
+  expect(author).toHaveTextContent("FreeCodeCamp");
+  expect(title).toHaveTextContent("Coding Blog");
 
   const likes = component.container.querySelector(".likes");
   //console.log(prettyDOM(likes));
-  expect(likes).toBeDefined();
+  expect(likes).toHaveTextContent("3");
+});
+
+it("clicking the like button twice, calls event handler function twice", async () => {
+  const blog = {
+    author: "FreeCodeCamp",
+    title: "Coding Blog",
+    likes: 3
+  };
+
+  const mockHandler = jest.fn();
+
+  const { getByText } = render(
+    <SimpleBlog blog={blog} onClick={mockHandler} />
+  );
+
+  const button = getByText("like");
+  fireEvent.click(button);
+  fireEvent.click(button);
+
+  expect(mockHandler.mock.calls.length).toBe(2);
 });
