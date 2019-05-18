5ba0197bdbb8382b0fcb2ee8cab1f3644a070f1f
Aija Valleala <aija.valleala@aalto.fi>
2019-05-18 16:07:21 +0300

    tehtävä 8.8

---
+++ osa8/library-frontend/src/components/Authors.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { Query } from "react-apollo";
+import { gql } from "apollo-boost";
+
+const ALL_AUTHORS = gql`
+  {
+    allAuthors {
+      name
+      born
+      bookCount
+    }
+  }
+`;
+
+const Authors = props => {
+  if (!props.show) {
+    return null;
+  }
+  const authors = [];
+
+  return (
+    <div>
+      <h2>authors</h2>
+
+      <Query query={ALL_AUTHORS}>
+        {result => {
+          if (result.loading) {
+            return <div>loading...</div>;
+          }
+          return (
+            <table>
+              <tbody>
+                <tr>
+                  <th />
+                  <th>born</th>
+                  <th>books</th>
+                </tr>
+                {result.data.allAuthors.map(a => (
+                  <tr key={a.name}>
+                    <td>{a.name}</td>
+                    <td>{a.born}</td>
+                    <td>{a.bookCount}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          );
+        }}
+      </Query>
+    </div>
+  );
+};
+
+export default Authors;
